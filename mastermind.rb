2f277e1db82ea767179bc017ca6a886fbdc6a39e
Guillermo <guillermoc93@protonmail.com>
2021-03-10 10:46:55 -0600

    computer iteration done

--- mastermind.rb
+++ mastermind.rb
@@ -57,21 +57,46 @@ class Game
     @turns = 1
   end
 
-  def start_game
-    puts 'Welcome to Mastermind!'
-    round
+  def play
+    start_game
+    game_loop
+    puts 'Thanks for playing!'
   end
 
-  def play_loop
-    round until @turns > 12 || round == 'OOOO'
-    # player.win if round == 'OOOO'
+  def start_game
+    puts 'Welcome to Mastermind!'
+    puts "\n"
+    puts 'An "O" means right number, right position!'
+    puts 'An "X" means right number, wrong position!'
+    puts "\n"
+  end
+
+  def game_loop
+    game_over = false
+    while game_over == false
+      while @turns <= 12
+        if win?(round) == true
+          player.win
+          break
+        end
+      end
+      if @turns > 12
+        player.loss
+        break
+      end
+      game_over = true
+    end
   end
 
   def round
     puts "Turn ##{@turns}. Make a guess:"
-    guess = @comp.feedback(player.guess)
-    puts guess
+    feedback = @comp.feedback(player.guess)
+    puts feedback
     @turns += 1
-    guess
+    feedback
+  end
+
+  def win?(feedback)
+    feedback == 'OOOO'
   end
 end
